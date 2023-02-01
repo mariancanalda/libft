a8e7b2685fe47e65dd9fbe036c45874a2ee9b8fe
Marian Canalda Criado <macanald@c1r13s2.42madrid.com>
2023-02-01 17:52:40 +0100

    Norminette a medias

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: macanald <macanald@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 16:41:41 by macanald          #+#    #+#             */
-/*   Updated: 2023/02/01 16:52:25 by macanald         ###   ########.fr       */
+/*   Updated: 2023/02/01 17:51:05 by macanald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
-	size_t 	total_size = count * size;
+	size_t	total_size;
+
+	total_size = count * size;
 	ptr = malloc(total_size);
-	if (ptr == NULL) 
+	if (ptr == NULL)
 	{
 		return (NULL);
-    }
+	}
 	for (size_t i = 0; i < total_size; i++) 
 	{
 		*((char*)ptr + i) = 0;
